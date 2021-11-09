deaaab3a9aa8451560e575aa933e50125d7a817e
Zdeněk Jurka <zdenek.jurka@jware.cz>
2021-11-09 11:36:23 +0100

    Možnost přidat SearchSerialier a Index pro extension

---
+++ src/Kdyby/DoctrineSearch/DI/ISearchSerializerProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace Kdyby\DoctrineSearch\DI;
+
+use Kdyby;
+use Nette;
+
+/**
+ * Mapping definition can be
+ * - absolute directory path __DIR__
+ * - array of absolute directory path [__DIR__]
+ * - DI\Statement instance with mapping type as entity new DI\Statement('annotations', [__DIR__])
+ *
+ */
+interface ISearchSerializerProvider
+{
+
+    /**
+     * Returns associative array of Namespace => mapping definition
+     *
+     * @return array
+     */
+    public function getSearchSerializerMappings();
+
+}
